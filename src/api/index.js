af77b4e8ca5d038c33ca45baaa7a55b02dfb0d18
Ankit Prajapati <55318994+script-beast@users.noreply.github.com>
2023-05-10 12:26:15 +0530

    Update backend

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mymemoriesserver.herokuapp.com/",
+  baseURL: "https://mymemoriesserver.onrender.com/",
 });
 
 api.interceptors.request.use((req) => {
